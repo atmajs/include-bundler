3861b21f5a6a18c17d469b9389f93849b7b2de82
Alex Kit <source.kit@gmail.com>
2017-04-19 13:34:55 +0200

    fix (mask deps resolver) correct type mappings

--- src/handlers/mask/MaskPathResolver.js
+++ src/handlers/mask/MaskPathResolver.js
@@ -1,23 +1,27 @@
-MaskHandler.PathResolver = class MaskPathResolver extends BasePathResolver {
+(function () {
 
-	constructor () {
-		super(...arguments);
-	}
-	resolve (includeData, resource) {
-		var node = {
-			path: includeData.url,
-			type: MAPPING[includeData.type]
-		};
-		var module = {
-			path: resource.url,
-			location: resource.location
-		};
-		return mask.Module.resolvePath(node, null, null, module);
-	}
-	accepts (includeData) {
-		return includeData.module === 'mask';
-	}
-};
+	MaskHandler.PathResolver = class MaskPathResolver extends BasePathResolver {
+
+		constructor () {
+			super(...arguments);
+		}
+		resolve (includeData, resource) {
+			var node = {
+				path: includeData.url,
+				type: MAPPING[includeData.type]
+			};
+			var module = {
+				path: resource.url,
+				location: resource.location
+			};
+			return mask.Module.resolvePath(node, null, null, module);
+		}
+		accepts (includeData) {
+			return includeData.module === 'mask';
+		}
+	};
 
 
-var MAPPING = { mask: 'mask', load: 'data', js: 'script', css: 'style' };
+	var MAPPING = { mask: 'mask', load: 'data', js: 'script', css: 'style' };
+
+}());
