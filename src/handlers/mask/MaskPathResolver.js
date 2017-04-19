37b2d17cafc0ced12123dba35daa0139f860346e
Alex Kit <source.kit@gmail.com>
2017-04-19 13:27:10 +0200

    add (PathResolver) mask modules

--- src/handlers/mask/MaskPathResolver.js
+++ src/handlers/mask/MaskPathResolver.js
@@ -3,8 +3,21 @@ MaskHandler.PathResolver = class MaskPathResolver extends BasePathResolver {
 	constructor () {
 		super(...arguments);
 	}
-	
-	accepts (type) {
-		return false;
+	resolve (includeData, resource) {
+		var node = {
+			path: includeData.url,
+			type: MAPPING[includeData.type]
+		};
+		var module = {
+			path: resource.url,
+			location: resource.location
+		};
+		return mask.Module.resolvePath(node, null, null, module);
+	}
+	accepts (includeData) {
+		return includeData.module === 'mask';
 	}
 };
+
+
+var MAPPING = { mask: 'mask', load: 'data', js: 'script', css: 'style' };
