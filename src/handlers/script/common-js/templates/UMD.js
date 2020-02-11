177e4fb6f0951041a4b14d69bd5ed91c59103816
Alex Kit <source.kit@gmail.com>
2020-02-11 23:13:42 +0100

    publish: fix (UMD) use commonjs first

--- src/handlers/script/common-js/templates/UMD.js
+++ src/handlers/script/common-js/templates/UMD.js
@@ -1,28 +1,31 @@
-(function(factory){
-	
-	var _name = '%NAME%',
-		_global = typeof window === 'undefined' ? global : window,
-		_module = {
-			exports: {}
-		};
+(function (factory) {
 
-	factory(_module, _module.exports, _global);
+    var _name = '%NAME%',
+        _global = typeof window === 'undefined' ? global : window,
+        _module = {
+            exports: {}
+        };
 
-	if (typeof define === 'function' && define.amd) {
-        define([], function () {
-        	return _module.exports;
-        });
-        return;
-    } 
+    factory(_module, _module.exports, _global);
+
     if (typeof module === 'object' && module.exports) {
-    	module.exports = _module.exports;
-    	return;
+        module.exports = _module.exports;
+        return;
     }
 
-	if (_name) {
-		_global[_name] = _module.exports;
-	}
+    if (typeof define === 'function' && define.amd) {
+        define([], function () {
+            return _module.exports;
+        });
+        return;
+    }
+    
+    if (_name) {
+        _global[_name] = _module.exports;
+    }
 
-}(function(module, exports, global){
-	%MODULE%
+}(function (module, exports, global) {
+
+    %MODULE%
+
 }));
